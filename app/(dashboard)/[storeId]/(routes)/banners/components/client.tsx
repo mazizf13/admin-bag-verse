785ab11fef362c64a2a5a82367170898bebf47c9
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-07-31 07:51:31 +0700

    feat: add data table component and update middleware for admin

--- app/(dashboard)/[storeId]/(routes)/banners/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/banners/components/client.tsx
@@ -3,12 +3,14 @@
 import { Button } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import { Banner } from '@prisma/client';
 import { Plus } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
+import { BannerColumn, columns } from './columns';
+import { DataTable } from '@/components/ui/data-table';
+import { ApiList } from '@/components/ui/api-list';
 
 interface BannerClientProps {
-  data: Banner[];
+  data: BannerColumn[];
 }
 
 export const BannerClient: React.FC<BannerClientProps> = ({ data }) => {
@@ -27,6 +29,10 @@ export const BannerClient: React.FC<BannerClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
+      <DataTable data={data} columns={columns} searchKey="label" />
+      <Heading title="API" description="API for Banners" />
+      <Separator />
+      <ApiList indicatorName="banners" indikatorId="bannerId" />
     </>
   );
 };
---
+++ components/ui/api-list.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useOrigin } from '@/hooks/use-origin';
+import { useParams } from 'next/navigation';
+import { ApiAlert } from './api-alert';
+
+interface ApiListProps {
+  indicatorName: string;
+  indikatorId: string;
+}
+
+export const ApiList: React.FC<ApiListProps> = ({
+  indicatorName,
+  indikatorId,
+}) => {
+  const params = useParams();
+  const origin = useOrigin();
+
+  const baseUrl = `${origin}/api/${params.storeId}`;
+
+  return (
+    <>
+      <ApiAlert
+        title="GET"
+        description={`${baseUrl}/${indicatorName}`}
+        variant="public"
+      />
+      <ApiAlert
+        title="GET"
+        description={`${baseUrl}/${indicatorName}/${indikatorId}`}
+        variant="public"
+      />
+      <ApiAlert
+        title="POST"
+        description={`${baseUrl}/${indicatorName}`}
+        variant="admin"
+      />
+      <ApiAlert
+        title="PATCH"
+        description={`${baseUrl}/${indicatorName}/${indikatorId}`}
+        variant="admin"
+      />
+      <ApiAlert
+        title="DELETE"
+        description={`${baseUrl}/${indicatorName}/${indikatorId}`}
+        variant="admin"
+      />
+    </>
+  );
+};
