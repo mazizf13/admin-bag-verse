6dd193badb65f266b81ba7b66c011e2b3a4ffda0
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-08-02 08:26:42 +0700

    feat: product

---
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -0,0 +1,292 @@
+'use client';
+
+import { AlertModal } from '@/components/modals/alert-modal';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Heading } from '@/components/ui/heading';
+import ImageUpload from '@/components/ui/image-upload';
+import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Separator } from '@/components/ui/separator';
+import { useOrigin } from '@/hooks/use-origin';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Category, Image, Product } from '@prisma/client';
+import axios from 'axios';
+import { Trash } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { z } from 'zod';
+
+interface ProductFormProps {
+  initialData:
+    | (Product & {
+        images: Image[];
+      })
+    | null;
+  categories: Category[];
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  images: z.object({ url: z.string() }).array(),
+  price: z.coerce.number().min(1),
+  categoryId: z.string().min(1),
+  isFeatured: z.boolean().default(false).optional(),
+  isArchived: z.boolean().default(false).optional(),
+});
+
+type ProductFormValues = z.infer<typeof formSchema>;
+
+export const ProductForm: React.FC<ProductFormProps> = ({
+  initialData,
+  categories,
+}) => {
+  const params = useParams();
+  const router = useRouter();
+  const origin = useOrigin();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? 'Edit Product' : 'Add Product';
+  const description = initialData ? 'Edit Product Store' : 'Add Product';
+  const toastMessage = initialData
+    ? 'Product updated successfully'
+    : 'Product added successfully';
+  const action = initialData ? 'Save Product' : 'Add Product';
+
+  const form = useForm<ProductFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData
+      ? {
+          ...initialData,
+          price: parseFloat(String(initialData?.price)),
+        }
+      : {
+          name: '',
+          images: [],
+          price: 0,
+          categoryId: '',
+          isFeatured: false,
+          isArchived: false,
+        },
+  });
+
+  const onSubmit = async (data: ProductFormValues) => {
+    try {
+      setLoading(true);
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/products/${params.productId}`,
+          data,
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/products`, data);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/products`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error('Check the input data again!');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/products/${params.productId}`);
+      router.refresh();
+      router.push(`/${params.storeId}/products`);
+      toast.success('Product deleted successfully');
+    } catch (error) {
+      toast.error('Check your data and connection again!');
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            variant="destructive"
+            disabled={loading}
+            size="sm"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="images"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Images</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    disabled={loading}
+                    onChange={(url) =>
+                      field.onChange([...field.value, { url }])
+                    }
+                    onRemove={(url) =>
+                      field.onChange([
+                        field.value.filter((current) => current.url !== url),
+                      ])
+                    }
+                    value={field.value.map((image) => image.url)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Product Name"
+                      disabled={loading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      placeholder="Rp"
+                      disabled={loading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <FormControl>
+                    <Select
+                      disabled={loading}
+                      onValueChange={(value) => field.onChange(value)}
+                      value={field.value}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select Category" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {categories.map((category) => (
+                          <SelectItem key={category.id} value={category.id}>
+                            {category.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="isFeatured"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>Featured</FormLabel>
+                    <FormDescription>
+                      This product will appear on the Home Page
+                    </FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="isArchived"
+              render={({ field }) => (
+                <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                  <FormControl>
+                    <Checkbox
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
+                  </FormControl>
+                  <div className="space-y-1 leading-none">
+                    <FormLabel>Archived</FormLabel>
+                    <FormDescription>
+                      This product will disappear from Store
+                    </FormDescription>
+                  </div>
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  );
+};
---
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -0,0 +1,157 @@
+import db from '@/lib/db';
+import { auth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  req: Request,
+  { params }: { params: { productId: string } },
+) {
+  try {
+    if (!params.productId) {
+      return new NextResponse('Product Id is required', { status: 400 });
+    }
+
+    const product = await db.product.findUnique({
+      where: {
+        id: params.productId,
+      },
+      include: {
+        images: true,
+        category: true,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log('[PRODUCT_GET]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } },
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    const body = await req.json();
+    const { name, price, categoryId, images, isFeatured, isArchived } = body;
+
+    if (!name) {
+      return new NextResponse('Product Name is required!', {
+        status: 400,
+      });
+    }
+
+    if (!images || !images.length) {
+      return new NextResponse('Image is required!', {
+        status: 400,
+      });
+    }
+
+    if (!price) {
+      return new NextResponse('Price is required!', {
+        status: 400,
+      });
+    }
+
+    if (!categoryId) {
+      return new NextResponse('Product Category is required!', {
+        status: 400,
+      });
+    }
+
+    if (!params.productId) {
+      return new NextResponse('Product Id is required', { status: 400 });
+    }
+
+    const storeByUserId = await db.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized');
+    }
+
+    await db.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        name,
+        price,
+        categoryId,
+        isFeatured,
+        isArchived,
+        images: {
+          deleteMany: {},
+        },
+      },
+    });
+
+    const product = await db.product.update({
+      where: {
+        id: params.productId,
+      },
+      data: {
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log('[PRODUCT_PATCH] Error:', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; productId: string } },
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse('Unauthenticated', { status: 401 });
+    }
+
+    if (!params.productId) {
+      return new NextResponse('Product Id is required', { status: 400 });
+    }
+
+    const storeByUserId = await db.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse('Unauthorized');
+    }
+
+    const product = await db.product.deleteMany({
+      where: {
+        id: params.productId,
+      },
+    });
+
+    return NextResponse.json(product);
+  } catch (error) {
+    console.log('[PRODUCT_DELETE]', error);
+    return new NextResponse('Internal error', { status: 500 });
+  }
+}
